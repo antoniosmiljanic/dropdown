15ea0fe602738ba1c95fcb0c5429f4f103c1f2ba
Antonio Smiljanić <antonio@umbeon.com>
2019-10-04 13:09:45 +0200

    Add other dropdown types

--- public/index.js
+++ public/index.js
@@ -1,43 +1,63 @@
-const NUM_OPTIONS = 35;
+const NUM_OPTIONS_LOTS = 35;
+const NUM_OPTIONS_LESS = 6;
+
 const CLASS_OPTIONS_HIDDEN = "options-hidden";
 const CLASS_OPTION = "option";
 
-const dropdown = document.getElementById("dropdown");
-const options = document.getElementsByClassName("options")[0];
-const textInput = document.getElementById("text-input");
+const dropdowns = document.getElementsByClassName("dropdown");
+const optionsLots = document.getElementsByClassName("options-lots");
+const optionsLess = document.getElementsByClassName("options-less");
 
 createDropdownOptions();
 
 function createDropdownOptions() {
-    for (i = 0; i < NUM_OPTIONS; i++) {
-        var option = document.createElement("div");
-        option.className = CLASS_OPTION;
-        option.innerHTML = `Option ${i}`;
-        option.onmousedown = onOptionMouseDown;
+  for (j = 0; j < optionsLots.length; j++) {
+    for (i = 0; i < NUM_OPTIONS_LOTS; i++) {
+      var option = document.createElement("div");
+      option.className = CLASS_OPTION;
+      option.innerHTML = `Option ${i}`;
+      option.onmousedown = onOptionMouseDown;
 
-        options.appendChild(option);
+      optionsLots[j].appendChild(option);
     }
+  }
+
+  for (j = 0; j < optionsLess.length; j++) {
+    for (i = 0; i < NUM_OPTIONS_LESS; i++) {
+      var option = document.createElement("div");
+      option.className = CLASS_OPTION;
+      option.innerHTML = `Option ${i}`;
+      option.onmousedown = onOptionMouseDown;
+
+      optionsLess[j].appendChild(option);
+    }
+  }
 }
 
 function onOptionMouseDown(event) {
-    textInput.value = event.target.innerText;
-    hideOptions();
+  const dropdown = event.target.closest(".dropdown");
+
+  const textInput = dropdown.querySelector(".text-input");
+
+  textInput.value = event.target.innerText;
+
+  hideOptions(dropdown);
 }
 
-function toggleOptions() {
-    const optionsHidden = dropdown.classList.contains(CLASS_OPTIONS_HIDDEN);
+function toggleOptions(dropdown) {
+  const optionsHidden = dropdown.classList.contains(CLASS_OPTIONS_HIDDEN);
 
-    if (optionsHidden) {
-        showOptions();
-    } else {
-        hideOptions();
-    }
+  if (optionsHidden) {
+    showOptions(dropdown);
+  } else {
+    hideOptions(dropdown);
+  }
 }
 
-function showOptions() {
-    dropdown.classList.remove(CLASS_OPTIONS_HIDDEN);
+function showOptions(dropdown) {
+  dropdown.classList.remove(CLASS_OPTIONS_HIDDEN);
 }
 
-function hideOptions() {
-    dropdown.classList.add(CLASS_OPTIONS_HIDDEN);
+function hideOptions(dropdown) {
+  dropdown.classList.add(CLASS_OPTIONS_HIDDEN);
 }
